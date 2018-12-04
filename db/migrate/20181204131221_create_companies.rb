a475ce865774ab4027d3b5bbc2dffecdbc1b96a7
Marion Schleifer <marion.schleifer@gmail.com>
2018-12-04 14:14:56 +0100

    company model

---
+++ db/migrate/20181204131221_create_companies.rb
@@ -0,0 +1,11 @@
+class CreateCompanies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :companies do |t|
+      t.string :name
+      t.string :website_url
+      t.string :logo_url
+
+      t.timestamps
+    end
+  end
+end
