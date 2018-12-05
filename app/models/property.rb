d597eb51e03ba9052698ecbcefaf3e860ddfba31
Marion Schleifer <marion.schleifer@gmail.com>
2018-12-05 15:41:09 +0100

    property validations

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,13 @@
 class Property < ApplicationRecord
+  #mandatory fields
+  validates :price, presence: true
+  validates :apn, presence: true
+
+  #positive fields
+  validates_numericality_of :price, :greater_than => 0.0
+  validates_numericality_of :number_of_stories, :greater_than => 0.0
+  validates_numericality_of :lot_size, :greater_than => 0.0
+
+  # number range
+  validates :year_built, inclusion: { in: 1500..2050 }
 end
