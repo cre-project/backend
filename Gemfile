0600b8f57c9da8813182905d4185b78eb9e272f8
Marion Schleifer <marion.schleifer@gmail.com>
2018-12-09 15:29:53 +0100

    use jbuilder for api

--- Gemfile
+++ Gemfile
@@ -45,3 +45,7 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# Jbuilder for API responses
+gem 'jbuilder', '~> 2.5'
+gem 'active_model_serializers', '~> 0.10.0'
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :property_units
   resources :properties
   resources :companies
-  resources :addresses
+  namespace 'api' do
+    resources :addresses
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
