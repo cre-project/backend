f89f8a18bc71771ddf8f1e8c42a5c1b935b65b34
Marion Schleifer <marion.schleifer@gmail.com>
2019-01-02 02:20:37 +0100

    heroku mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { :host => 'https://cre-backend-testing.herokuapp.com' }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
