9af2ef8e536093207e6ecd9c3d0b8cf10de97222
Marion Schleifer <marion.schleifer@gmail.com>
2019-01-06 15:22:51 +0100

    fix pabbly api

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -8,25 +8,31 @@ module Api
     end
 
     def create
-      pabbly_customer_id = params[:data][:customer_id]
-      subscription_expiration = params[:data][:expiry_date]
-      subscription = params[:data][:plan][:plan_code]
+      if params[:data][:customer_id].present? && params[:data][:expiry_date].present? && params[:data][:plan][:plan_code].present?
+        pabbly_customer_id = params[:data][:customer_id]
+        subscription_expiration = params[:data][:expiry_date]
+        subscription = params[:data][:plan][:plan_code]
 
-      auth = { username: ENV["PABBLY_API_KEY"], password: ENV["PABBLY_SECRET_KEY"] }
-      pabbly_user = HTTParty.get('https://payments.pabbly.com/api/v1/customer/' + pabbly_customer_id, basic_auth: auth)
+        auth = { username: ENV["PABBLY_API_KEY"], password: ENV["PABBLY_SECRET_KEY"] }
+        pabbly_user = HTTParty.get('https://payments.pabbly.com/api/v1/customer/' + pabbly_customer_id, basic_auth: auth)
+        if pabbly_user.present?
+          company_name = pabbly_user["data"]["company_name"]
+          first_name = pabbly_user["data"]["first_name"]
+          last_name = pabbly_user["data"]["last_name"]
+          email = pabbly_user["data"]["email_id"]
 
-      company_name = pabbly_user["data"]["company_name"]
-      first_name = pabbly_user["data"]["first_name"]
-      last_name = pabbly_user["data"]["last_name"]
-      email = pabbly_user["data"]["email_id"]
-
-      @user = User.create(email: email, first_name: first_name, last_name: last_name, pabbly_customer_id: pabbly_customer_id, subscription_expiration: subscription_expiration, subscription: subscription)
-      @user.company = Company.create(name: company_name)
-
-      if @user.present?
-        render json: @user, status: :created
+          @user = User.create(email: email, first_name: first_name, last_name: last_name, pabbly_customer_id: pabbly_customer_id, subscription_expiration: subscription_expiration, subscription: subscription)
+          @user.company = Company.create(name: company_name)
+        else
+          logger.info 'Customer cannot be created'
+        end
       else
-        render json: @user.errors, status: :unprocessable_entity
+        logger.info 'Customer cannot be created'
+      end
+      if @user
+        render json: @user, status: :ok
+      else
+        render body: nil, status: :ok
       end
     end
 
