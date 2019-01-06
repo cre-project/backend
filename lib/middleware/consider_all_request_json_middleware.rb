1e8c8d85c5b9ead70ccea75e66d98d228c8f2020
Marion Schleifer <marion.schleifer@gmail.com>
2019-01-06 20:10:34 +0100

    debug

--- lib/middleware/consider_all_request_json_middleware.rb
+++ lib/middleware/consider_all_request_json_middleware.rb
@@ -1,12 +1,12 @@
 class ConsiderAllRequestJsonMiddleware
-  def initialize app
-    @app = app
-  end
+  # def initialize app
+  #   @app = app
+  # end
 
-  def call(env)
-    if env["CONTENT_TYPE"] == 'text/plain'
-      env["CONTENT_TYPE"] = 'application/json'
-    end
-    @app.call(env)
-  end
+  # def call(env)
+  #   if env["CONTENT_TYPE"] == 'text/plain'
+  #     env["CONTENT_TYPE"] = 'application/json'
+  #   end
+  #   @app.call(env)
+  # end
 end
