eba469ce104b78a758f9bebcc81c4a10afac2733
Marion Schleifer <marion.schleifer@gmail.com>
2019-01-08 14:41:29 +0100

    remove debugger

--- app/mailers/password_reset_mailer/password_reset_mailer.rb
+++ app/mailers/password_reset_mailer/password_reset_mailer.rb
@@ -4,7 +4,6 @@ class PasswordResetMailer < ApplicationMailer
   def reset_password_email
     @user = params[:user]
     @url  = '<dashboard-url>/reset/' + @user.reset_password_token
-    binding.pry
     mail(to: @user.email, subject: 'Password Reset Offering CRE')
   end
 end
