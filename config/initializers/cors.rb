707894859abf5e62e3882007256ee4514f23f082
Marion Schleifer <marion.schleifer@gmail.com>
2019-01-11 10:51:10 +0100

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080'
-    origins 'cre-dashboard-testing.firebaseapp.com'
+    origins 'localhost:8080', 'cre-dashboard-testing.firebaseapp.com'
 
     resource '*',
       headers: :any,
