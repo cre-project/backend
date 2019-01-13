0600287605ce22dcee515a1a2eec68eb811f4383
Marion Schleifer <marion.schleifer@gmail.com>
2019-01-13 11:02:07 +0100

    make package rented units user specific

--- app/controllers/api/package_rented_units_controller.rb
+++ app/controllers/api/package_rented_units_controller.rb
@@ -3,33 +3,66 @@ module Api
     before_action :set_package_rented_unit, only: [:show, :update, :destroy]
 
     def index
-      render json: PackageRentedUnit.all.order('created_at DESC')
+      if @current_user.present?
+        @package = @current_user.packages.find_by(id: package_rented_unit_params[:package_id])
+        @rented_unit = @current_user.rented_units.find_by(id: package_rented_unit_params[:rented_unit_id])
+        render json: PackageRentedUnit.where(package_id: @package.id, rented_unit_id: @rented_unit.id)
+      else
+        render body: nil, status: :forbidden
+      end
     end
 
     def show
-      render json: @package_rented_unit
+      @package = @current_user.packages.find_by(id: package_rented_unit_params[:package_id])
+      @rented_unit = @current_user.rented_units.find_by(id: package_rented_unit_params[:rented_unit_id])
+      if @current_user.present? && @package.user_id == @current_user.id && @rented_unit.user_id == @current_user.id
+        render json: @package_rented_unit
+      else
+        render body: nil, status: :forbidden
+      end
     end
 
     def create
-      @package_rented_unit = PackageRentedUnit.new(package_rented_unit_params)
+      if @current_user.present?
+        @package = @current_user.packages.find_by(id: package_rented_unit_params[:package_id])
+        @rented_unit = @current_user.rented_units.find_by(id: package_rented_unit_params[:rented_unit_id])
+        @package_rented_unit = @package.package_rented_units.build(package_rented_unit_params)
+        @package_rented_unit = @rented_unit.package_rented_units.build(package_rented_unit_params)
 
-      if @package_rented_unit.save
-        render json: @package_rented_unit, status: :created
+        if @package_rented_unit.save
+          render json: @package_rented_unit, status: :created
+        else
+          render json: @package_rented_unit.errors, status: :unprocessable_entity
+        end
       else
-        render json: @package_rented_unit.errors, status: :unprocessable_entity
+        render body: nil, status: :forbidden
       end
     end
 
     def update
-      if @package_rented_unit.update(package_rented_unit_params)
-        render json: @package_rented_unit, status: :ok
+      @package = @current_user.packages.find_by(id: package_rented_unit_params[:package_id])
+      @rented_unit = @current_user.rented_units.find_by(id: package_rented_unit_params[:rented_unit_id])
+
+      if @current_user.present? && @package.user_id == @current_user.id && @rented_unit.user_id == @current_user.id
+        if @package_rented_unit.update(package_rented_unit_params)
+          render json: @package_rented_unit, status: :ok
+        else
+          render json: @package_rented_unit.errors, status: :unprocessable_entity
+        end
       else
-        render json: @package_rented_unit.errors, status: :unprocessable_entity
+        render body: nil, status: :forbidden
       end
     end
 
     def destroy
-      @package_rented_unit.destroy
+      @package = @current_user.packages.find_by(id: package_rented_unit_params[:package_id])
+      @rented_unit = @current_user.rented_units.find_by(id: package_rented_unit_params[:rented_unit_id])
+
+      if @current_user.present? && @package.user_id == @current_user.id && @rented_unit.user_id == @current_user.id
+        @package_rented_unit.destroy
+      else
+        render body: nil, status: :forbidden
+      end
     end
 
     private
