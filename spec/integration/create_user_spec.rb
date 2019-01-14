47b868a7eae0e96f8d1e4e9cba5ae09b1515fe3d
Marion Schleifer <marion.schleifer@gmail.com>
2019-01-14 10:53:50 +0100

    fix user spec #33

--- spec/integration/create_user_spec.rb
+++ spec/integration/create_user_spec.rb
@@ -5,55 +5,32 @@ require "spec_helper"
 class CreateUserTest < ActionDispatch::IntegrationTest
 
   RSpec.describe "user subscription", :type => :request do
-    request_body = JSON.parse('{
-      "event_type": "subscription_activate",
+    request_body = {
+      "event_type": "customer_create",
       "event_source": "checkout",
       "data": {
-        "plan": {
-          "plan_active": "true",
-          "bump_offer": {},
-          "createdAt": "2018-10-01T11:42:29.036Z",
-          "updatedAt": "2018-10-01T11:42:29.036Z",
-          "id": "5bb2082501605a3642b6f767",
-          "product_id": "5bb2081801605a3642b6f766",
-          "user_id": "5b961bc55dfff7797d9cd897",
-          "plan_name": "one time",
-          "plan_code": "one-time",
-          "price": 100,
-          "billing_period": "",
-          "billing_period_num": "",
-          "billing_cycle": "onetime",
-          "billing_cycle_num": null,
-          "trial_period": 0,
-          "setup_fee": 0,
-          "plan_description": ""
-        },
-        "setup_fee": null,
-        "payment_terms": "",
-        "currency_symbol": "$",
-        "payment_method": "5bbee98b458d576755a217d5",
-        "cron_process": "done",
-        "createdAt": "2018-10-11T06:11:22.711Z",
-        "updatedAt": "2018-10-11T06:11:22.711Z",
-        "id": "5bbee98a458d576755a217d2",
-        "customer_id": "5bbee98a458d576755a217d1",
-        "product_id": "5bb2081801605a3642b6f766",
-        "plan_id": "5bb2082501605a3642b6f767",
-        "amount": 100,
-        "email_id": "testwebhook2@gmail.com",
-        "status": "live",
-        "quantity": 1,
-        "starts_at": "2018-10-11T06:11:22.222Z",
-        "activation_date": "2018-10-11T06:11:24.242Z",
-        "expires_at": null,
-        "expiry_date": "2118-10-11T06:11:22.222Z",
-        "trial_days": 0,
-        "trial_expiry_date": "",
-        "next_billing_date": "",
-        "last_billing_date": "2018-10-11T06:11:24.242Z"
+        "company_name": "Marion Inc.",
+        "shipping_address": {},
+        "createdAt": "2019-01-08T19:27:24.449Z",
+        "updatedAt": "2019-01-08T19:27:24.449Z",
+        "id": "5c34f99cfbfefc3720cc653b",
+        "user_id": "5c164645ef788b4507f7ced8",
+        "first_name": "Marion",
+        "last_name": "Schleifer",
+        "user_name": "marionwerwerw@gmail.com",
+        "email_id": "marionwerwerw@gmail.com",
+        "billing_address": {},
+        "credit": nil,
+        "pcustomer_id": "",
+        "api": nil,
+        "client_portal"=>false,
+        permitted: false
       },
-      "create_time": "2018-10-11T06:11:25.184Z"
-    }').to_json
+      "create_time": "2019-01-08T19:27:24.2424+00:00",
+      "controller": "api/users",
+      "action": "create",
+      "user": {}
+    }.to_json
 
     it "creates user" do
       post "/api/users", params: request_body, headers: { 'CONTENT_TYPE' => 'application/json' }
