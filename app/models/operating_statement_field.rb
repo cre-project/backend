13c4a0e6a2920a4dbb6f9b058e450f297a04b4c7
Marion Schleifer <marion.schleifer@gmail.com>
2019-01-27 19:40:29 +0100

    fix operating statement fields validation

--- app/models/operating_statement_field.rb
+++ app/models/operating_statement_field.rb
@@ -1,4 +1,5 @@
 class OperatingStatementField < ApplicationRecord
-  belongs_to :operating_statement
+  belongs_to :operating_statement, dependent: :destroy
   validates :name, presence: true
+  validates_inclusion_of :is_income, in: [true, false]
 end
