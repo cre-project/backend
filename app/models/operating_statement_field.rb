37739ce876c18877ac0757febeebdf10e5cb54a5
Marion Schleifer <marion.schleifer@gmail.com>
2019-01-27 19:45:15 +0100

    remove dependent destroy

--- app/models/operating_statement_field.rb
+++ app/models/operating_statement_field.rb
@@ -1,5 +1,5 @@
 class OperatingStatementField < ApplicationRecord
-  belongs_to :operating_statement, dependent: :destroy
+  belongs_to :operating_statement
   validates :name, presence: true
   validates_inclusion_of :is_income, in: [true, false]
 end
